44e4a539548ab43d6e6636e4169e5ca18789ad57
govarthanan291-svg <govarthanan291@gmail.com>
2026-02-14 13:36:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 
 @st.cache_resource
 def load_model():
-    return joblib.load("models/mushroom_gb_model.pkl")
+    return joblib.load("mushroom_gb_model.pkl")
 
 model = load_model()
 
